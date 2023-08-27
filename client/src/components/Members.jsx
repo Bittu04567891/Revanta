6936f9488da012f87dcd968a90ee43efb6a66ba8
Bittu <kumarbittu775@gmail.com>
2023-08-27 20:21:34 +0530

    routing

--- client/src/components/Members.jsx
+++ client/src/components/Members.jsx
@@ -1,29 +1,42 @@
-import styled from "styled-components"
-import { popularProducts } from "../data"
-import Member from "./Member"
-
-const Container=styled.div`
-   padding: 20px;
-   display: flex;
-   flex-wrap:wrap;
-   justify-content:space-between;
-    
-`
-const Wrapper=styled.header`
-    font-weight: 600;
-    font-size: large;
-`
+import styled from "styled-components";
+import { heads } from "../data";
+import Member from "./Member";
+import { Link } from "react-router-dom";
+import {
+  ArrowRightAltOutlined,
+  ChevronRightOutlined,
+} from "@mui/icons-material";
+const Arrow = styled.div`
+  padding-top: 4cm;
+  font-weight: 600;
+`;
+const Container = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+const Wrapper = styled.header`
+  font-weight: 600;
+  font-size: large;
+`;
 
 const Members = () => {
+  // const handleReload = () => {
+  //   window.location.reload(); // Reload the page
+  // };
   return (
     <Container>
-       <Wrapper>Team Leads</Wrapper>
-       {popularProducts.map(item=>(
-        <Member item={item} key={item.id}/>
-       ))}
-       
+      {heads?.map((item) => (
+        <Member item={item} key={item.id} />
+      ))}
+      <Arrow>
+        <Link to="/Revanta/members">
+          <ChevronRightOutlined />
+        </Link>
+      </Arrow>
     </Container>
-  )
-}
+  );
+};
 
-export default Members
+export default Members;
